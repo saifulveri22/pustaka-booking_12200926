3fb5017127ef4eb17219de957df5a7a985b628c1
saifulveri22 <saifulveri22@gmail.com>
2021-10-01 14:17:34 +0700

    Tugas Pertemuan 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,11 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/hello', 'SelamatDatang::hal_awal');
+$routes->get('/login', 'SelamatDatang::beranda_login');
+$routes->post('/login', 'Login::cekLogin');
+$routes->get('/daftar-member', 'SelamatDatang::daftar_member');
+
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,28 @@
+<?=$this->extend('dasar_tampilan');?>
+<?=$this->section('isiweb')?>
+<form method='POST' action='<?=base_url('/login')?>'>
+    <div class="container col-md-3">
+        <div class="card mt-5">
+            <div class='card-header'>
+                <h3 class='card-title'>Login</h3>
+            </div>
+            <div class="card-body">
+                <div class="form-floating">
+                    <input id="txtEmail" class="form-control"
+                            placeholder="Email"
+                            type="text" name="email" />
+                    <label for='txtEmail'>Email</label>
+                </div>
+                <div class="form-floating">
+                    <input id="txtPass" class="form-control"
+                            placeholder='Password'
+                            type='password' name='sandi' />
+                    <label for='txtPass'>Kata Sandi</label>
+                </div>
+
+                <button class="btn btn-primary mt-2">Login</button>
+            </div>
+        </div>
+    </div>
+</form>
+<?=$this->endSection()?>
