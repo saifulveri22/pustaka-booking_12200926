f82d1acd0174a59bdd280233d4e17ec1ad052420
saifulveri22 <saifulveri22@gmail.com>
2021-11-15 12:11:55 +0700

    Tugas Pertemuan 8

--- app/Controllers/SelamatDatang.php
+++ app/Controllers/SelamatDatang.php
@@ -15,7 +15,8 @@ class SelamatDatang extends BaseController
             'vd' => $this->session->getFlashdata('validator'),
             'email' => $this->session->get('email'),
             'sandi' => $this->session->get('sandi'),
-            'error' => $this->session->getFlashdata('error')
+            'error' => $this->session->getFlashdata('error'),
+            'title' => 'Login sisfo Perpustakaan'
         ]);
     }
     public function daftar_member()
